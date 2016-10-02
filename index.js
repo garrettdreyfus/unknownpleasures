c9d0e4e743a877c0b40dd0299340a2c485bbd00e
Garrett Finucane <garrettdreyfus@gmail.com>
2016-10-02 13:29:23 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); 
+canvas.width = 800; //document.width is obsolete
+canvas.height = 800;
+ctx.lineWidth = 3;
+
+var fps = 120;
+var n = 0;
+animate();
+
+function centerAmplitude(x){
+      var temp =  Math.min((.5 / Math.abs(x - canvas.width/2)), 0.01);
+			return temp;
+
+}
+// sawtooth sine
+function dumbrandom(x, a, b, c, amplitude) {
+  return 300 * amplitude * (5 * Math.sin(.1 * (x + a)) + 3 * Math.sin(0.3333333 * (x + b)) + 0.25 * Math.sin(4 * (x + c)));
+}
+
+var waves= [];
+var num=40;
+var excluded = 2;
+for(var i=excluded; i<num-excluded;i++){
+	waves.push(new Wave(dumbrandom,centerAmplitude,(canvas.width/num)*i));
+}
+function animate() {
+  setTimeout(function() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+		for(var i=0; i<num-2*excluded;i++){
+			waves[i].draw();
+		}
+
+
+  }, 100 / fps);
+}
+
---
+++ wave.js
@@ -0,0 +1,24 @@
+function Wave(wav, amp, placement) {
+  this.Aoffset = Math.random()*20;
+  this.Boffset = Math.random()*30;
+  this.Coffset = Math.random()*20;
+  this.shift = 0;
+  this.edge = 100;
+  this.wave = wav;
+  this.amplitude = amp;
+  this.placement = placement;
+  this.draw = function(){
+    // Drawing code goes here
+    n = canvas.width;
+    this.shift++;
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    for (var x = this.edge; x < n-this.edge; x++) {
+      var y = this.wave((x - this.shift)/2, this.Aoffset, this.Boffset, this.Coffset, this.amplitude(x));
+      ctx.lineTo(x, y + this.placement + 50 * (Math.abs(x - canvas.width/2) / n));
+    }
+    ctx.stroke();
+  }
+}
